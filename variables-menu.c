2e0ec3fe9674b1c988c5f0794bc6566b1a223660
Luna Abigail Martinez Robles <lunabisakura@gmail.com>
2025-04-04 08:29:01 -0600

    Update variables-menu.c

--- variables-menu.c
+++ variables-menu.c
@@ -3,10 +3,12 @@
 char main(){
     char snack1[] = "Chips";
     float price1 = 1.50;
-    int stock = 20;
+    int stock1 = 20;
 
     printf("Welcome to the Snack Shop!\n");
     printf("---------------------------\n");
+    printf("1. %s - $%.2f - %d Available\n", snack1, price1, stock1);
+
     
     return 0;
 }
