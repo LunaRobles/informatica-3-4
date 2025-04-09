20afb73f0bd5bc351b39ef84f42e7aafb1dee353
Luna Abigail Martinez Robles <lunabisakura@gmail.com>
2025-04-09 16:31:38 +0000

    va

--- variables-operations.c
+++ variables-operations.c
@@ -1,25 +1,43 @@
 #include <stdio.h>
 
 int main(){
-    int length = 4;
-    int with = 10;
-    int area = length * with;
-    int perimeter = (length + length + with + with);
-    printf("area: %d \n",area);
-    printf("perimeter: %d \n",perimeter);
+    int length;
+    printf("enter the length of the rectangle:");
+     scanf("%d",&length)
+   
+     int with;
+    printf("enter the with of the rectangle:");
+    scanf("%d",&with)
 
-    int fahrenheit = 40;
+    int perimeter = (length + length + with + with);
+    printf("perimeter: %d \n",perimeter);
+    int area = length * with;
+    printf("area: %d \n",area);
+
+
+    int fahrenheit;
+    printf("Enter an integer:");
+    scanf("%d",&fahrenheit)
+
     float celsius = (fahrenheit-32)/1.8;
     printf("fahrenheit: %d \n",fahrenheit);
     printf("celsius: %2f \n",celsius);
 
-    int totalminutes = 349;
+
+    int totalminutes;
+    printf("Enter an integer:");
+    scanf("%d",&totalminutes)
+
     int hours = totalminutes/60;
-    int minutes = 349 - 300;
+    int minutes = totalminutes - (hours*60);
     printf("%d minutes is equal to %d hours and %d minutes \n",totalminutes, hours, minutes);
 
     float pi = 3.14;
-    int radio = 3;
+    int radio;
+    printf("Enter an integer:");
+    scanf("%d",&radio)
+
+
     int diameter = radio*2;
     float areac = diameter * pi;
     printf("the area of a circle that it's diameter is %d, will be: %2f", diameter, areac);
