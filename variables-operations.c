f4b6e9ab35706a06e9beea3cb4df4aff6d9695a5
Luna Abigail Martinez Robles <lunabisakura@gmail.com>
2025-04-09 14:50:53 +0000

    si

--- variables-operations.c
+++ variables-operations.c
@@ -16,7 +16,13 @@ int main(){
     int totalminutes = 349;
     int hours = totalminutes/60;
     int minutes = 349 - 300;
-    printf("%d minutes is equal to %d hours and %d minutes",totalminutes, hours, minutes);
+    printf("%d minutes is equal to %d hours and %d minutes \n",totalminutes, hours, minutes);
+
+    float pi = 3.14;
+    int radio = 3;
+    int diameter = radio*2;
+    float areac = diameter * pi;
+    printf("the area of a circle that it's diameter is %d, will be: %2f", diameter, areac);
 
     return 0;
 }
