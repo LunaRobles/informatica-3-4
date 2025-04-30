66b34e87e5f305f32adf983118dd50af8819edd6
Luna Abigail Martinez Robles <lunabisakura@gmail.com>
2025-04-30 18:14:37 +0000

    años

--- IfElseChallenge.c
+++ IfElseChallenge.c
@@ -2,13 +2,13 @@
 
     int main (){
     int year;
-    printf (enter a year);
+    printf ("enter a year");
     scanf("%d", &year);
 
     if ((year %4== 0 && year % 100 != 0) || year%400 == 0) {
-        printf("%d is a leap year.\n", year);
-    if ((year %4== 0 && year % 100 != 0) || year%400 == 0)
-        printf("%d is not a leap year.\n", year);
+        printf("%d is a leap year.\n", year);}
+    else{
+        printf("%d is not a leap year.\n", year);}
     }
 
-    }
+    
