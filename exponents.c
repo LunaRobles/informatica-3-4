8b21eff40f07070e6552730dc9a28ee0d526c1b9
Luna Abigail Martinez Robles <lunabisakura@gmail.com>
2025-05-08 16:32:07 +0000

    me deja escribir

---
+++ exponents.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int base;
+    printf("Enter the base number:");
+    scanf("%d",&base);
+
+    int exponent;
+    printf("Enter the exponent:");
+    scanf("%d",&exponent);
+
+    int i=1;
+
+    do {base * base;
+    }
+     while (i=exponent);
+    
+    
+    printf("%d ^ %d\n", base,exponent);
+     return 0;
+    }
