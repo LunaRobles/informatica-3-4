e8ad225f2a2f656988957cfb59a7efe0195431f6
Luna Abigail Martinez Robles <lunabisakura@gmail.com>
2025-05-09 14:55:20 +0000

    ya

--- exponents.c
+++ exponents.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
 
 int main(){
+
+    //While loop exponent
     int base;
     printf("Enter the base number:");
     scanf("%d",&base);
+    
 
     int exponent;
     printf("Enter the exponent:");
     scanf("%d",&exponent);
 
     int i=1;
-
-    do {base * base;
+    int result = base;
+    do { result= base * result;
+        i++;
     }
-     while (i=exponent);
+     while (i<exponent);
     
     
-    printf("%d ^ %d\n", base,exponent);
+    printf("%d ^ %d= %d\n", base,exponent, result);
+
+    //C Library Functions
+
+
+
+
+
+
      return 0;
     }
