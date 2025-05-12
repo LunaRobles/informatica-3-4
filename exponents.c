c0e333ba468ac0ed1e3e3ce2453699efd26d26b3
Luna Abigail Martinez Robles <lunabisakura@gmail.com>
2025-05-12 16:31:23 +0000

    hola profe

--- exponents.c
+++ exponents.c
@@ -1,33 +1,35 @@
 #include <stdio.h>
-
-int main(){
-
-    //While loop exponent
-    int base;
-    printf("Enter the base number:");
-    scanf("%d",&base);
+   #include <math.h>
+int main()
+//  {While loop exponent
+//  int base;
+//  printf("Enter the base number:");
+//  scanf("%d",&base);
     
 
-    int exponent;
-    printf("Enter the exponent:");
-    scanf("%d",&exponent);
-
-    int i=1;
-    int result = base;
-    do { result= base * result;
-        i++;
-    }
-     while (i<exponent);
+//  int exponent;
+//    printf("Enter the exponent:");
+//    scanf("%d",&exponent);
+//
+//    int i=1;
+//    int result = base;
+//    do { result= base * result;
+//       i++;
+    
+//     while (i<exponent);
     
     
-    printf("%d ^ %d= %d\n", base,exponent, result);
-
+//    printf("%d ^ %d= %d\n", base,exponent, result);
+{
     //C Library Functions
+    float num, root;
+    printf("enter a number:");
+     scanf("%f", &num);
 
+        //Computes the squarew root of num and stores in root.
+        root= sqrt (num);
 
+        printf("square root of %.2f = %.2f", num, root);
 
-
-
-
-     return 0;
-    }
+    return 0;
+ }
