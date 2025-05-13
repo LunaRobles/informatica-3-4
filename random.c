89dda66e43d240b4fee79ebf8bff51c6ec4d8bb1
Luna Abigail Martinez Robles <lunabisakura@gmail.com>
2025-05-13 14:51:34 +0000

    no le entiendo

---
+++ random.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    srand(time(NULL));
+for{
+    printf("Roll i: %d\n", diceRoll);
+}
+do{
+    int diceRoll = (rand() % 6) + 1; // Generates a number between 1 and 6
+}
+
+
+    int j;
+    int multi=1;
+    for (j=1; j<m+1; ++j){
+        multi= multi*j;
+        }
